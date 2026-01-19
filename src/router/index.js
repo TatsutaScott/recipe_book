44aaf233e739fc5562153c898aacc4c551525f35
TatsutaScott <tatsuta.scott@gmail.com>
2026-01-19 11:13:28 +0900

    added in basic table of contents

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import recipe_view from '@/views/recipe_view.vue'
-
+import TOC_view from '@/views/TOC.vue'
 const routes = [
-  {
-    path: '/recipe/:slug',
-    component: recipe_view,
-  },
+  { path: '/recipe/:slug', component: recipe_view },
+  { path: '/TOC', component: TOC_view },
 ]
 
 const router = createRouter({
