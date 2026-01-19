205c15b4f3b1b107280d6e47a8c4de3f42a9c5c1
TatsutaScott <tatsuta.scott@gmail.com>
2026-01-19 10:52:01 +0900

    added nav component

---
+++ src/components/nav_comp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="nav_container">
+    <img src="@/assets/icons/home_icon.svg" alt="" />
+    <img src="@/assets/icons/menu_icon.svg" alt="" />
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.nav_container {
+  width: auto;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
