52df6eebea195c792dc7c951a51b9772bee138ca
TatsutaScott <tatsuta.scott@gmail.com>
2026-01-19 11:13:14 +0900

    updated to use material icons isntead of svg files

--- src/components/nav_comp.vue
+++ src/components/nav_comp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nav_container">
-    <img src="@/assets/icons/home_icon.svg" alt="" />
-    <img src="@/assets/icons/menu_icon.svg" alt="" />
+    <a class="link" href="/"><span class="material-icons">home</span></a>
+    <a class="link" href="/TOC"><span class="material-icons">menu</span></a>
   </div>
 </template>
 
@@ -12,5 +12,17 @@
   width: auto;
   display: flex;
   justify-content: space-between;
+}
+
+.link {
+  padding: 0.5em;
+  border-radius: 10px;
+  color: black;
+  transition: all 0.5s;
+  line-height: 10pt;
+}
+
+.link:hover {
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
 }
 </style>
