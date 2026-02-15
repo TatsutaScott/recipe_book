240e7b7e814358b37be911ca1eecf63dc3632ada
TatsutaScott <tatsuta.scott@gmail.com>
2026-02-15 14:06:02 +0900

    added notes comp

---
+++ src/components/notes_comp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="note_container">
+    <div v-for="(note, index) in props.notes" :key="note">
+      ※{{index}}: {{ note }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({notes: { type: Array}});
+</script>
+
+<style scoped>
+.note_container{
+  color:gray;
+  font-family: Neue_Montreal;
+  font-size: 11pt;
+  font-style: italic;
+}
+
+</style>
